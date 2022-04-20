e08cf494717575dcd932ade1809146e5cf6b50e7
Maressa Alves <maressa.msoa@gmail.com>
2022-04-20 10:47:48 -0300

    creating simple calculator

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -7,9 +7,6 @@ public class PrimeiroPrograma {
 		Livros livros = new Livros();
 		
 		System.out.println(cachorro);
-		/*int a = 2;
-		int b = 3;
-		System.out.println("Hello World! " + (a+b));*/	
 		}
 
 }
